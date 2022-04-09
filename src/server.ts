412d1ff0e3804a6718d7936cb6e956c74ecbd227
ronaldobergara <bergaraheadbanger@gmail.com>
2022-04-09 20:34:55 -0300

    metodos HTTP GET POST PUT DELETE

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,29 @@
-import express from "express";
+import express, { response } from "express";
 
 const app = express()
+
+// Métodos HTTP -> GET | POST | PUT | DELETE
+
+//http://localhost:3333/users
+
+// request  = requisição
+// response = retorno de uma resposta para o front end por exemplo
+
+app.get('/users', (request, response) => {
+    return response.json(['usuário 1', 'usuário 2'])
+})
+
+app.post('/users', (request, response) => {
+    return response.json({ message: 'Criando usuário' })
+})
+
+app.put('/users', (request, response) => {
+    return response.json({ message: 'Atualizando usuário'})
+})
+
+app.delete('/users', (request, response) => {
+    return response.json({ message: 'Excluindo usuário' })
+})
 
 app.listen('3333', () => {
     console.log('Back-end Started!')
