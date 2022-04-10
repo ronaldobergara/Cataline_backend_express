24dc970483dbc396487afa1bb79e55af5ad01803
ronaldobergara <bergaraheadbanger@gmail.com>
2022-04-10 10:09:00 -0300

    Utilizando parametros Query Params, Route Params e request Body

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express, { response } from "express";
 
 const app = express()
 
+app.use(express.json())
+
 // Métodos HTTP -> GET | POST | PUT | DELETE
 
 //http://localhost:3333/users
@@ -9,15 +11,30 @@ const app = express()
 // request  = requisição
 // response = retorno de uma resposta para o front end por exemplo
 
+
 app.get('/users', (request, response) => {
+
+    const {perPage, page} = request.query
+
+    console.log(perPage)
+    console.log(page)
+
     return response.json(['usuário 1', 'usuário 2'])
 })
 
 app.post('/users', (request, response) => {
+    const body = request.body
+
+    console.log(body)
+
     return response.json({ message: 'Criando usuário' })
 })
 
-app.put('/users', (request, response) => {
+app.put('/users/:id', (request, response) => {
+    const { id } = request.params
+
+    console.log(id)
+
     return response.json({ message: 'Atualizando usuário'})
 })
 
